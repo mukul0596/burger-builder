3b2bc4c4af7f7dda9266bd885bdfcee88106b13c
mukul0596 <icecoolmkl@gmail.com>
2019-09-03 03:03:30 +0530

    added sidedrawer

--- src/component/Logo/Logo.js
+++ src/component/Logo/Logo.js
@@ -3,7 +3,7 @@ import logoUrl from '../../assets/images/burger-logo.png';
 import './Logo.css';
 
 const logo = (props) => (
-    <div className='Logo'>
+    <div className='Logo' style={{ height: props.height }}>
         <img src={ logoUrl } alt='MyBurger'/>
     </div>
 )
