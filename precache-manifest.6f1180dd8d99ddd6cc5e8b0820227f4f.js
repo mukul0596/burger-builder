ee9d4aa888cc79d3277ae9738b619cade0120b8c
mukul0596 <icecoolmkl@gmail.com>
2019-09-03 04:25:35 +0530

    Updates

---
+++ precache-manifest.6f1180dd8d99ddd6cc5e8b0820227f4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04122e692a315b4c01781df3f5817cc",
+    "url": "/burger-builder-v2.0/index.html"
+  },
+  {
+    "revision": "2d5355a5a0f0b54c9dfa",
+    "url": "/burger-builder-v2.0/static/css/main.d32b3744.chunk.css"
+  },
+  {
+    "revision": "4a3564205a50492c4825",
+    "url": "/burger-builder-v2.0/static/js/2.f2c756ed.chunk.js"
+  },
+  {
+    "revision": "2d5355a5a0f0b54c9dfa",
+    "url": "/burger-builder-v2.0/static/js/main.0a5dc629.chunk.js"
+  },
+  {
+    "revision": "04dde3d702b0a291422a",
+    "url": "/burger-builder-v2.0/static/js/runtime~main.08ffe1da.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder-v2.0/static/media/burger-logo.b8503d26.png"
+  }
+]);
